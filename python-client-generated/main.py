3b7486776e66f6451af6bf6f4f2e16aee588a01b
16pierre <quentin.pierre@mines-paristech.fr>
2021-03-01 20:49:12 +0100

    It works

--- python-client-generated/main.py
+++ python-client-generated/main.py
@@ -4,11 +4,14 @@ from swagger_client.api.default_api import DefaultApi
 from swagger_client.models.message import Message
 
 
-config = Configuration(host="localhost")
+# Un peu de configuration, pas besoin de toucher
+
+config = Configuration()
+config.host = 'https://messagerie.tech/'
 client = ApiClient(configuration=config)
 api = DefaultApi(api_client=client)
 
-
+# Code pour envoyer des requetes
 message = Message(contenu="Hello world", emetteur="Quentin")
 api.messages_post(body=message)
 
